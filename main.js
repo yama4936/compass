4239eb687b29d1bb3014372aee3151df1c4c68b9
yama4936 <t323088@chukyo-u.ac.jp>
2024-12-07 21:35:15 +0900

    js

--- main.js
+++ main.js
@@ -1,28 +1,128 @@
-window.addEventListener('deviceorientation', function (event) {
-    console.log('方角       : ' + event.alpha);
-    console.log('上下の傾き : ' + event.beta);
-    console.log('左右の傾き : ' + event.gamma);
-
-    console.log('コンパスの向き : ' + event.webkitCompassHeading);
-    console.log('コンパスの精度 : ' + event.webkitCompassAccuracy);
-});
-
-// 簡易OS判定
-function detectOSSimply() {
-    let ret;
+// OSの判定
+function judgOS() {
+    let os;//userAgentで読み込んでindexofで探している
     if (
         navigator.userAgent.indexOf("iPhone") > 0 ||
         navigator.userAgent.indexOf("iPad") > 0 ||
         navigator.userAgent.indexOf("iPod") > 0
     ) {
         // iPad OS13以上のsafariはデフォルト「Macintosh」なので別途要対応
-        ret = "iphone";
+        os = "iphone";
     } else if (navigator.userAgent.indexOf("Android") > 0) {
-        ret = "android";
+        os = "android";
     } else {
-        ret = "pc";
+        os = "pc";
     }
 
-    return ret;
+    if (os == "iphone") {
+        // safari用。DeviceOrientation APIの使用をユーザに許可して貰う
+        document.querySelector("#permit").addEventListener("click", permitDeviceOrientationForSafari);
+
+        window.addEventListener(
+            "deviceorientation",
+            orientation,
+            true
+        );
+    } else if (os == "android") {
+        window.addEventListener(
+            "deviceorientationabsolute",
+            orientation,
+            true
+        );
+    } else {
+        window.alert("PC未対応サンプル");
+    }
+}
+// iPhone + Safariの場合はDeviceOrientation APIの使用許可をユーザに求める
+function permitDeviceOrientationForSafari() {
+    DeviceOrientationEvent.requestPermission()
+        .then(response => {
+            if (response === "granted") {
+                window.addEventListener(
+                    "deviceorientation",
+                    detectDirection
+                );
+            }
+        })
+        .catch(console.error);
 }
 
+// ジャイロスコープと地磁気をセンサーから取得
+function orientation(event) {
+    let absolute = event.absolute;
+    let alpha = event.alpha;
+    let beta = event.beta;
+    let gamma = event.gamma;
+
+    let degrees;
+    if (os == "iphone") {
+        // webkitCompasssHeading値を採用
+        degrees = event.webkitCompassHeading;
+
+    } else {
+        // deviceorientationabsoluteイベントのalphaを補正
+        degrees = compassHeading(alpha, beta, gamma);
+    }
+
+    let direction;
+    if (
+        (degrees > 337.5 && degrees < 360) ||
+        (degrees > 0 && degrees < 22.5)
+    ) {
+        direction = "北";
+    } else if (degrees > 22.5 && degrees < 67.5) {
+        direction = "北東";
+    } else if (degrees > 67.5 && degrees < 112.5) {
+        direction = "東";
+    } else if (degrees > 112.5 && degrees < 157.5) {
+        direction = "東南";
+    } else if (degrees > 157.5 && degrees < 202.5) {
+        direction = "南";
+    } else if (degrees > 202.5 && degrees < 247.5) {
+        direction = "南西";
+    } else if (degrees > 247.5 && degrees < 292.5) {
+        direction = "西";
+    } else if (degrees > 292.5 && degrees < 337.5) {
+        direction = "北西";
+    }
+
+    document.querySelector("#direction").innerHTML =
+        direction + " : " + degrees;
+    document.querySelector("#absolute").innerHTML = absolute;
+    document.querySelector("#alpha").innerHTML = alpha;
+    document.querySelector("#beta").innerHTML = beta;
+    document.querySelector("#gamma").innerHTML = gamma;
+}
+
+// 端末の傾き補正（Android用）
+// https://www.w3.org/TR/orientation-event/
+function compassHeading(alpha, beta, gamma) {
+    let degtorad = Math.PI / 180; // Degree-to-Radian conversion
+
+    let _x = beta ? beta * degtorad : 0; // beta value
+    let _y = gamma ? gamma * degtorad : 0; // gamma value
+    let _z = alpha ? alpha * degtorad : 0; // alpha value
+
+    let cX = Math.cos(_x);
+    let cY = Math.cos(_y);
+    let cZ = Math.cos(_z);
+    let sX = Math.sin(_x);
+    let sY = Math.sin(_y);
+    let sZ = Math.sin(_z);
+
+    // Calculate Vx and Vy components
+    let Vx = -cZ * sY - sZ * sX * cY;
+    let Vy = -sZ * sY + cZ * sX * cY;
+
+    // Calculate compass heading
+    let compassHeading = Math.atan(Vx / Vy);
+
+    // Convert compass heading to use whole unit circle
+    if (Vy < 0) {
+        compassHeading += Math.PI;
+    } else if (Vx < 0) {
+        compassHeading += 2 * Math.PI;
+    }
+
+    return compassHeading * (180 / Math.PI); // Compass Heading (in degrees)
+}
